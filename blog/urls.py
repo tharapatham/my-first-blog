4bf7a79b1dab8ca0a82a5e42c9a00cd3a31530db
tharapatham <susanmol@yahoo.com>
2020-02-20 10:42:39 -0600

    Restored the empty path to show homepage.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
     
 urlpatterns = [
-    path('posts/', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
